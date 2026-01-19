6dbd80752a76321016bbfe93857a164d841929e4
AliHaider7108 <110810805+AliHaider7108@users.noreply.github.com>
2026-01-19 14:16:49 +0500

    dummy code for git
    fadfadsf

---
+++ ParkingArea.h
@@ -0,0 +1,14 @@
+// code 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+
+    cout << "Hello, World!" << endl;
+    cout << "Sum of a and b is: " << sum << endl;
+
+    return 0;
+}
