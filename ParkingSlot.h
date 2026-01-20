adeb4bdf9c181d78a20d661e2cd5c80313921185
AliHaider7108 <110810805+AliHaider7108@users.noreply.github.com>
2026-01-20 17:12:14 +0500

    parking

---
+++ ParkingSlot.h
@@ -0,0 +1,25 @@
+
+#ifndef PARKING_SLOT_H
+#define PARKING_SLOT_H
+
+class ParkingSlot
+{
+private:
+    int slotId;
+    int zoneId;
+    bool isAvailable;
+
+public:
+    ParkingSlot(int slotId, int zoneId);
+
+    int getSlotId() const;
+    void setSlotId(int slotId);
+
+    int getZoneId() const;
+    void setZoneId(int zoneId);
+
+    bool getIsAvailable() const;
+    void setIsAvailable(bool isAvailable);
+};
+
+#endif  
